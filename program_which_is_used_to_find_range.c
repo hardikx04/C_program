0c121c9bf1052e4ec6b2c51abb290d70c9446f5e
hardikx04 <113246442+hardikx04@users.noreply.github.com>
2023-08-07 23:28:58 +0530

    Add files via upload

---
+++ program_which_is_used_to_find_range.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <float.h>
+#include <limits.h>
+int main()
+{ 
+ 
+ printf("min range of int is %e \n", INT_MIN);
+ printf("max range of int is %e \n", INT_MAX);
+ printf("min range of SHORT is %e \n", SHRT_MIN);
+ printf("max range of SHORT is %e \n", SHRT_MAX);
+ printf("min range of FLOAT is %e \n", FLT_MIN);
+ printf("max range of FLOAT is %e \n", FLT_MAX);
+ printf("min range of CHAR is %e \n", CHAR_MIN);
+ printf("max range of CHAR is %e \n", CHAR_MAX);
+ return 0;
+}
